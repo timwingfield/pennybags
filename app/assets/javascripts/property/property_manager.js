437c00c900658b7447a50ce9a66552a37cb5fe1b
Tim Wingfield <tim@timwingfield.com>
2013-07-12 16:48:24 -0400

    added property details view and functionality

--- app/assets/javascripts/property/property_manager.js
+++ app/assets/javascripts/property/property_manager.js
@@ -13,10 +13,11 @@ var Properties = Backbone.Collection.extend({
 var PropertiesView = Backbone.View.extend({
 
   initialize: function(){
-    _.bindAll(this, "render");
+    _.bindAll(this, "render", "loadPropertyDetails");
     this.collection = new Properties();
 
     this.collection.on('reset', this.render, this);
+    Backbone.on('property:selected', this.loadPropertyDetails);
 
     this.collection.fetch({reset: true});
   },
@@ -28,15 +29,43 @@ var PropertiesView = Backbone.View.extend({
     }, this);
 
     return this;
+  },
+
+  loadPropertyDetails: function(property) {
+    var $propertyDetails
+    $propertyDetails = $('.property-details');
+
+    this.propertyDetails = this.propertyDetails || new PropertyDetailsView();
+    this.propertyDetails.model = property;
+
+    $propertyDetails.html(this.propertyDetails.render().el);
   }
 });
 
 var SmallCardView = Backbone.View.extend({
   template: _.template($("#small-card").html()),
 
+  events: {
+    'click' : 'selectProperty'
+  },
+
   render: function(){
     html = this.template(this.model.toJSON());
     $(this.el).append(html); 
+    return this;
+  },
+
+  selectProperty: function(){
+    Backbone.trigger('property:selected', this.model);
+  }
+});
+
+var PropertyDetailsView = Backbone.View.extend({
+  template: _.template($("#property-details").html()),
+
+  render: function(){
+    html = this.template(this.model.toJSON());
+    this.$el.html(html); 
     return this;
   }
 });
