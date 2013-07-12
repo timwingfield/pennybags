c1a2befd16246e8256e8bb4d8b8c5611d0c03143
Tim Wingfield <tim@timwingfield.com>
2013-07-12 15:53:42 -0400

    added view

--- app/assets/javascripts/property/property_manager.js
+++ app/assets/javascripts/property/property_manager.js
@@ -9,3 +9,19 @@ var Properties = Backbone.Collection.extend({
   model: Property,
   url: '/properties'
 });
+
+var PropertiesView = Backbone.View.extend({
+
+  initialize: function(){
+    this.collection = new Properties();
+
+    this.collection.on('reset', this.render, this);
+
+    this.collection.fetch({reset: true});
+  },
+
+  render: function(){
+    // loop over collection
+    return this;
+  }
+});
