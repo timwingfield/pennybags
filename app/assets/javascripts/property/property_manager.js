45de160e7734c3134756d728d39ad171dd0970f1
Tim Wingfield <tim@timwingfield.com>
2013-07-12 15:50:08 -0400

    add model and collection

--- app/assets/javascripts/property/property_manager.js
+++ app/assets/javascripts/property/property_manager.js
@@ -2,3 +2,10 @@ _.templateSettings = {
   interpolate: /\{\{\=(.+?)\}\}/g,
   evaluate: /\{\{(.+?)\}\}/g
 };
+
+var Property = Backbone.Model.extend({});
+
+var Properties = Backbone.Collection.extend({
+  model: Property,
+  url: '/properties'
+});
